868735a7884e0d987bb65980570143eec679443f
leo <liuguoxin.good@gmail.com>
2022-06-17 22:48:36 -0400

    added timer and localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,18 @@ var Choices = document.querySelector("#choice-list");
 var taskIdCounter = 1;
 var pageContentEl = document.querySelector("#page-content");
 var Qcount=1;
+var score=0;
+var pageContentEl = document.querySelector("#choice-list");
+var timerEl = document.getElementById('countdown');
+
+var timeLeft = 100;
+var scoreDataObj = {
+  name: "",
+  time: "",
+}
 
 var createQuestion =function(Qnumber){
-  var QLibrary=["click to start quiz!","quiz1","quiz2","quiz3","quiz4","quiz5","end"];
+  var QLibrary=["Coding Quiz Challenge!","Quiz1:correct answer is 1","Quiz2:correct answer is 2","Quiz3:correct answer is 3","Quiz4:correct answer is 4","Quiz5:correct answer is 1","All Done!"];
   var question = document.querySelector("#question");
   var oneQuestion = document.createElement("p");
   oneQuestion.textContent = QLibrary[Qnumber];
@@ -20,11 +29,11 @@ var createChoice=function (Cnumber){
     oneChoice.className = "task-item";
     oneChoice.setAttribute("data-task-id", 0);
     Choices.appendChild(oneChoice);
-  }else {
+  }else if (Cnumber==2||Cnumber==3||Cnumber==4||Cnumber==5){
     var abc=['1','2','3','4'];
     for (var i=1;i<=4;i++){
       var oneChoice = document.createElement("li");
-      oneChoice.textContent = abc[i-1]+"."+"hello";
+      oneChoice.textContent = abc[i-1]+"."+"Answer";
       oneChoice.className = "task-item";
       oneChoice.setAttribute("data-task-id", i);
       Choices.appendChild(oneChoice);
@@ -40,20 +49,95 @@ var createResult =function (taskId) {
       oneResult.className = "one-result";
       oneResult.setAttribute("data-task-id", 200);
       resultDisplay.appendChild(oneResult);
-    }else if (taskId=="1"){
-      var resultDisplay = document.querySelector("#result");
-      var oneResult = document.createElement("p");
-      oneResult.textContent = "Correct";
-      oneResult.className = "one-result";
-      oneResult.setAttribute("data-task-id", 200);
-      resultDisplay.appendChild(oneResult);
-    } else{
+    }else {
+      if (Qcount=="1"){
+        if (taskId=="1")  {
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Correct";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+          score++;
+        } else{
           var resultDisplay = document.querySelector("#result");
           var oneResult = document.createElement("p");
           oneResult.textContent = "Incorrect";
           oneResult.className = "one-result";
           oneResult.setAttribute("data-task-id", 200);
           resultDisplay.appendChild(oneResult);
+        }
+      }else if (Qcount=="2"){
+        if (taskId=="2")  {
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Correct";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+          score++;
+        } else{
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Incorrect";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+        }
+      }else if (Qcount=="3"){
+        if (taskId=="3")  {
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Correct";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+          score++;
+        } else{
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Incorrect";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+        }
+      }else if (Qcount=="4"){
+        if (taskId=="4")  {
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Correct";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+          score++;
+        } else{
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Incorrect";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+        }
+        
+      }else if(Qcount=="5"){
+        if (taskId=="1")  {
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Correct";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+          score++;
+        } else{
+          var resultDisplay = document.querySelector("#result");
+          var oneResult = document.createElement("p");
+          oneResult.textContent = "Incorrect";
+          oneResult.className = "one-result";
+          oneResult.setAttribute("data-task-id", 200);
+          resultDisplay.appendChild(oneResult);
+        }
+      }
+        
     }
 }; 
 
@@ -66,7 +150,55 @@ var deleteTask = function(taskId)   {
 };
 
 var quizEnd = function(){
-  createQuestion(6);
+ 
+  var endText = document.createElement("li");
+  endText.textContent = "Your final score is"+score+".";
+  endText.setAttribute("data-task-id", 300);
+  Choices.appendChild(endText);
+  var endText = document.createElement("li");
+  endText.textContent = "Enter initials:";
+  endText.setAttribute("data-task-id", 400);
+
+  var text15 =document.createElement('input');
+  text15.setAttribute('type', 'text');//输入框的类型
+  text15.setAttribute('id', "initial");//输入框的id
+  endText.appendChild(text15);
+
+  var editButtonEl = document.createElement("button");
+  editButtonEl.textContent = "Submit";
+  editButtonEl.className = "btn edit-btn";
+  editButtonEl.type="submit";
+  editButtonEl.setAttribute("data-task-id", 50);
+  endText.appendChild( editButtonEl);
+  Choices.appendChild(endText);
+  
+  timerEl.remove();
+  scoreDataObj.time=timeLeft;
+  savedScore.push(scoreDataObj);
+  
+}
+function countdown() {
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+     
+    }
+  }, 1000);
 }
 
 createQuestion(0);
@@ -85,40 +217,65 @@ var generateQuiz=function(taskId) {
     createChoice(2);
     createResult(taskId);
   }else if (taskId=="1"||taskId=="2"||taskId=="3"||taskId=="4"){
-    if (Qcount <= 4){
+    if (Qcount <= 5){
       var taskSelected = document.querySelector(".one-question[data-task-id='" + 100 + "']");
       taskSelected.remove();
       var taskSelected = document.querySelector(".one-result[data-task-id='" + 200 + "']");
       taskSelected.remove();
-      deleteTask(1);
-      deleteTask(2);
-      deleteTask(3);
-      deleteTask(4);
+      for (var i=1;i<=4;i++){
+        deleteTask(i);
+      }
       createQuestion(Qcount+1);
       createChoice(Qcount+1);
       createResult(taskId);
-      Qcount++;
-    }else{
-      var taskSelected = document.querySelector(".one-question[data-task-id='" + 100 + "']");
-      taskSelected.remove();
-      var taskSelected = document.querySelector(".one-result[data-task-id='" + 200 + "']");
-      taskSelected.remove();
-      deleteTask(1);
-      deleteTask(2);
-      deleteTask(3);
-      deleteTask(4);
-      quizEnd();
+      if (Qcount==5){
+        
+        var taskSelected = document.querySelector(".one-result[data-task-id='" + 200 + "']");
+        taskSelected.remove();
+        quizEnd();
+      }
+      
     }
     
-  }  
-      
+    Qcount++;
+    
+  }  else if (taskId=="50"){
+    var firstNameInput = document.querySelector("#initial");
+    scoreDataObj.name=firstNameInput.value.trim();
+    saveScore();
+  }     
 }
 
+var savedScore=[];
+var idNumber=0;
+var saveScore = function() {
 
+  
+  localStorage.setItem("tasks", JSON.stringify(savedScore));
+};
 
+var loadTasks = function() {
+  tasks = localStorage.getItem("tasks");
 
-// startQuiz();
+  if (!tasks) {
+    return false;
+  }
+
+  tasks = JSON.parse(tasks);
+  for (var i = 0; i < savedTasks.length; i++) {
+      // pass each task object into the `createTaskEl()` function
+      createTaskEl(savedTasks[i]);
+  }
+}
+var textChanges=function(){
+ 
+}
+countdown();
+
 pageContentEl.addEventListener("click", startQuiz);
+
+
+
 
 
 
